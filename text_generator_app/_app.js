88afe176b29f6a4de5659201bfd299b14118a902
Anna Ågren <info@annaagren.se>
2021-01-08 14:47:18 +0100

    new name

---
+++ text_generator_app/_app.js
@@ -0,0 +1,11 @@
+import { AnimateSharedLayout } from "framer-motion";
+import "../index.css";
+
+// This default export is required in a new `pages/_app.js` file.
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <AnimateSharedLayout>
+      <Component {...pageProps} />
+    </AnimateSharedLayout>
+  );
+}
