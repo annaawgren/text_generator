e3d0d582d51c65b6554f980da0f6f3a1c5455b73
Pär Thernström <par.thernstrom@gmail.com>
2021-02-24 11:33:27 +0100

    Make about modal work

--- text_generator_app/components/Footer.js
+++ text_generator_app/components/Footer.js
@@ -1,42 +1,42 @@
 import React, { useState, useEffect } from "react";
 
 export default function Footer(props) {
-  const [open, setOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
 
   const handleOpen = () => {
-    setOpen({ aboutVisible: !open.aboutVisible });
+    setIsOpen(!isOpen);
   };
 
   return (
     <>
       <footer className="w-full flex flex-col justify-between items-center py-40 text-green">
-        <div
-          className={`contact-info ${
-            open.mobileMenuVisible ? "visible" : "hidden"
-          }`}
-        >
-          <div className="about w-full h-screen bg-red flex flex-col items-center justify-between pt-60 pb-20 ">
-            <div onClick={handleOpen} className="w-2 h-2 bg-beige">
-              closetag
-            </div>
-            <div className="text-center w-full ">
-              <p className="text-md">
-                The DRAIKE Job Rapplication is an AI-powered job application
-                generator trained on Drake lyrics and public resumes.
-                <br />
-                It creates unique and useless cover letters. It is a project by
-                <a
-                  href="https://newbietech.io"
-                  target="_blank"
-                  className="text-red"
-                >
-                  Newbie Tech Exploration Lab"
-                </a>
-                , made with OpenAI's GTP-2 and ML Runway.
-              </p>
+        {isOpen && (
+          <div className={`contact-info`}>
+            <div className="about fixed inset-0	w-full h-screen bg-red flex flex-col items-center justify-between pt-60 pb-20 ">
+              <div onClick={handleOpen} className="w-2 h-2 bg-beige">
+                closetag
+              </div>
+              <div className="text-center w-full ">
+                <p className="text-md">
+                  The DRAIKE Job Rapplication is an AI-powered job application
+                  generator trained on Drake lyrics and public resumes.
+                  <br />
+                  It creates unique and useless cover letters. It is a project
+                  by
+                  <a
+                    href="https://newbietech.io"
+                    target="_blank"
+                    className="text-red"
+                  >
+                    Newbie Tech Exploration Lab"
+                  </a>
+                  , made with OpenAI's GTP-2 and ML Runway.
+                </p>
+              </div>
             </div>
           </div>
-        </div>
+        )}
+
         <button
           className="outline-none focus:outline-none text-green hover:text-red duration-300 shadow"
           onClick={handleOpen}
